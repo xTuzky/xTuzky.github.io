5d515193ca4f7f6922423920001cba28968f59b3
xTuzky <65010487+xTuzky@users.noreply.github.com>
2022-03-08 23:43:26 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+let image = document.querySelector('img');
+
+image.onclick = function()
+{
+
+	let mySrc = image.getAttribute('src');
+
+	/* Set the new source */
+	if(mySrc === 'images/chaos.jpg') {
+		image.setAttribute('src', 'images/blackchaos.png');
+	} else {
+		image.setAttribute('src', 'images/chaos.jpg');
+	}
+}
+
+/* Lets store h1 and button */
+let myHead = document.querySelector('h1');
+let myButton = document.querySelector('button');
+
+/* Create function for it */
+function setUserName() {
+
+	let myName = prompt("Please enter your name.");
+	if(!myName)
+	{
+		setUserName();
+	}
+	else {
+		
+		localStorage.setItem("name", myName);
+		myHead.textContent = "Hello, " + myName;
+	}
+
+}
+
+if(!localStorage.getItem("name"))
+{
+	setUserName();
+} else {
+	let myNameNow = localStorage.getItem("name");
+	myHead.textContent = "Hello, " + myNameNow;	
+}
+
+myButton.onclick = function()
+{
+	setUserName();
+}
+
